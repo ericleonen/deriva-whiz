098f21eb5df49460bc3d9f63a68026925fb8465e
ericleonen <ericleonen@gmail.com>
2024-03-18 14:49:53 -0700

    create home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,20 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div></div>
+    <div className="w-screen h-screen flex flex-col">
+      <section className="w-screen flex-grow flex justify-center items-center flex-col">
+        <h1 className="text-4xl font-bold">Deriva-Whiz</h1>
+        <Link 
+          href="/play"
+          className="mt-3 text-2xl font-medium bg-green-400/30 text-green-500 py-2 px-8 rounded-md"
+        >
+          Play
+        </Link>
+      </section>
+      <footer className="w-full py-3 flex justify-center">
+        Built by <Link href="https://github.com/ericleonen" className="underline text-blue-500 ml-1">ericleonen</Link>
+      </footer>
+    </div>
   )
 }
